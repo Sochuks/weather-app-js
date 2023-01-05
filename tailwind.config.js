dbc2164c1e96ff7f56b3b8daa870fa611571f3ee
Sochuks <moseschukwumakajang@gmail.com>
2023-01-05 12:45:44 +0100

    - Modified index - Added weather-app - Added city-time & weather to weather-app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+  },
+  extend: {
+    colors: {
+      primaryCol: 'hsla(234, 100%, 66%, 1)',
+      secCol: 'hsla(232, 100%, 74%, 1)',
+      thirdCol: 'hsla(227, 100%, 80%, 1)',
+      fourCol: 'hsla(218, 100%, 87%, 1)',
+      fiveCol: 'hsla(184, 100%, 94%, 1)',
+    }
+  },
   },
   plugins: [],
 }
